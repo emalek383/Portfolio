c3528960328e788c1f6d040fee49cb640635f736
emalek383 <166419206+emalek383@users.noreply.github.com>
2024-07-18 13:56:23 +0200

    Add files via upload

--- data_loader.py
+++ data_loader.py
@@ -7,11 +7,24 @@ Created on Sat Jul 13 09:30:31 2024
 #import appdirs as ad
 #ad.user_cache_dir = lambda *args: "/tmp"
 import yfinance as yf
+from contextlib import contextmanager
+import sys
+import io
 
 yf.pdr_override()
 
 # Disable progress bar
 yf.set_tz_cache_location(None)
+
+@contextmanager
+def suppress_stdout():
+    new_stdout = io.StringIO()
+    old_stdout = sys.stdout
+    sys.stdout = new_stdout
+    try:
+        yield
+    finally:
+        sys.stdout = old_stdout
 
 def get_data(stocks, start, end):
     """
@@ -25,7 +38,21 @@ def get_data(stocks, start, end):
     Returns:
         stockData: pd.DataFrame with close of stock data
     """
+    try:
+        # Disable progress bar
+        yf.pdr_override()
+        yf.set_tz_cache_location(None)
+        
+        with suppress_stdout():
+            stockData = yf.download(stocks, start=start, end=end, progress=False)
+        
+        if stockData.empty:
+            raise ValueError("No data downloaded. Please check your inputs and internet connection.")
     
-    stockData = yf.download(stocks, start, end)
-    stockData = stockData['Close']
-    return stockData
+        #stockData = yf.download(stocks, start, end)
+        stockData = stockData['Close']
+        return stockData
+    except Exception as e:
+        print(f"An error occurred while downloading data: {str(e)}")
+        # You might want to return None or an empty DataFrame here
+        return None
