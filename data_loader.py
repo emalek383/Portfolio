5fd280c2c18c25951643c2bd4a31cc130009af33
emalek383 <166419206+emalek383@users.noreply.github.com>
2024-07-18 13:53:56 +0200

    Add files via upload

--- data_loader.py
+++ data_loader.py
@@ -4,9 +4,14 @@ Created on Sat Jul 13 09:30:31 2024
 
 @author: emanu
 """
-import appdirs as ad
-ad.user_cache_dir = lambda *args: "/tmp"
+#import appdirs as ad
+#ad.user_cache_dir = lambda *args: "/tmp"
 import yfinance as yf
+
+yf.pdr_override()
+
+# Disable progress bar
+yf.set_tz_cache_location(None)
 
 def get_data(stocks, start, end):
     """
