d226cd7706488b5036bb0363f2d86f6bf197cf38
emalek383 <166419206+emalek383@users.noreply.github.com>
2024-07-18 13:57:50 +0200

    Add files via upload

--- data_loader.py
+++ data_loader.py
@@ -11,11 +11,6 @@ from contextlib import contextmanager
 import sys
 import io
 
-yf.pdr_override()
-
-# Disable progress bar
-yf.set_tz_cache_location(None)
-
 @contextmanager
 def suppress_stdout():
     new_stdout = io.StringIO()
@@ -38,21 +33,12 @@ def get_data(stocks, start, end):
     Returns:
         stockData: pd.DataFrame with close of stock data
     """
-    try:
-        # Disable progress bar
-        yf.pdr_override()
-        yf.set_tz_cache_location(None)
+    # Disable progress bar
+    yf.pdr_override()
+    yf.set_tz_cache_location(None)
         
-        with suppress_stdout():
-            stockData = yf.download(stocks, start=start, end=end, progress=False)
+    with suppress_stdout():
+        stockData = yf.download(stocks, start=start, end=end, progress=False)
         
-        if stockData.empty:
-            raise ValueError("No data downloaded. Please check your inputs and internet connection.")
-    
-        #stockData = yf.download(stocks, start, end)
-        stockData = stockData['Close']
-        return stockData
-    except Exception as e:
-        print(f"An error occurred while downloading data: {str(e)}")
-        # You might want to return None or an empty DataFrame here
-        return None
+    stockData = stockData['Close']
+    return stockData
