76aaa950c37a5e51103d17a3ff8b27e6f41a55d1
emalek383 <166419206+emalek383@users.noreply.github.com>
2024-07-18 13:47:17 +0200

    Add files via upload

--- data_loader.py
+++ data_loader.py
@@ -4,7 +4,8 @@ Created on Sat Jul 13 09:30:31 2024
 
 @author: emanu
 """
-
+import appdirs as ad
+ad.user_cache_dir = lambda *args: "/tmp"
 import yfinance as yf
 
 def get_data(stocks, start, end):
