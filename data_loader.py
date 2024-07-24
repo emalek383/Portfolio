02a770683a2832392370755ddd6685d9e1e8806d
emalek383 <166419206+emalek383@users.noreply.github.com>
2024-07-24 16:02:54 +0200

    Add files via upload

--- data_loader.py
+++ data_loader.py
@@ -1,36 +1,32 @@
-#import appdirs as ad
-#ad.user_cache_dir = lambda *args: "/tmp"
-import yfinance as yf
-from contextlib import contextmanager
-import sys
-import io
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jul 13 09:30:31 2024
 
-#@contextmanager
-#def suppress_stdout():
-#    new_stdout = io.StringIO()
-#    old_stdout = sys.stdout
-#    sys.stdout = new_stdout
-#    try:
-#        yield
-#    finally:
-#        sys.stdout = old_stdout
+@author: emanu
+"""
+import yfinance as yf
+
 
 def download_data(stocks, start, end):
     """
     Download stock data from yahoo finance.
-    
-    Args:
-        stocks: list of stock tickers
-        start: start date in datetime format
-        end: end date in datetime format
-    
-    Returns:
-        stockData: pd.DataFrame with close of stock data
+
+    Parameters
+    ----------
+    stocks : list(str)
+        List of stock tickers.
+    start : datetime
+        Start date.
+    end : datetime
+        End date.
+
+    Returns
+    -------
+    stockData : pd.DataFrame
+        Dataframe containing the closing price of the stock data.
+
     """
-#    #yf.pdr_override()
-#    yf.set_tz_cache_location(None)
-        
- #   with suppress_stdout():
+    
     stockData = yf.download(stocks, start=start, end=end, progress=False)
         
     stockData = stockData['Close']
