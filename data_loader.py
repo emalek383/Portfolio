cabcd69f037816c4b46b418fcc11471bd3609b7a
emalek383 <166419206+emalek383@users.noreply.github.com>
2024-07-24 15:58:09 +0200

    Update data_loader.py

--- data_loader.py
+++ data_loader.py
@@ -5,15 +5,15 @@ from contextlib import contextmanager
 import sys
 import io
 
-@contextmanager
-def suppress_stdout():
-    new_stdout = io.StringIO()
-    old_stdout = sys.stdout
-    sys.stdout = new_stdout
-    try:
-        yield
-    finally:
-        sys.stdout = old_stdout
+#@contextmanager
+#def suppress_stdout():
+#    new_stdout = io.StringIO()
+#    old_stdout = sys.stdout
+#    sys.stdout = new_stdout
+#    try:
+#        yield
+#    finally:
+#        sys.stdout = old_stdout
 
 def download_data(stocks, start, end):
     """
@@ -27,11 +27,11 @@ def download_data(stocks, start, end):
     Returns:
         stockData: pd.DataFrame with close of stock data
     """
-    #yf.pdr_override()
-    yf.set_tz_cache_location(None)
+#    #yf.pdr_override()
+#    yf.set_tz_cache_location(None)
         
-    with suppress_stdout():
-        stockData = yf.download(stocks, start=start, end=end, progress=False)
+ #   with suppress_stdout():
+    stockData = yf.download(stocks, start=start, end=end, progress=False)
         
     stockData = stockData['Close']
     return stockData
