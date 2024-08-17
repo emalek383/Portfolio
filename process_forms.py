72876616fee4e360388df8c1a9f58d598e09ba82
emalek383 <166419206+emalek383@users.noreply.github.com>
2024-08-17 19:36:50 +0200

    Add files via upload

--- process_forms.py
+++ process_forms.py
@@ -103,7 +103,8 @@ def process_stock_form(stock_list = None, start_date = None, end_date = None, ri
         for stock in stocks:
             stock = stock.strip()
             if stock:
-                cleaned_stocks.add(stock.upper())
+                stock = stock.upper()
+                cleaned_stocks.add(stock)
         
         # If stocks, start and end date are the same as loaded, just use the loaded data
         if (state.universe and state.universe.stocks and set(state.universe.stocks) == cleaned_stocks and 
